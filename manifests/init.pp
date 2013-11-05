eb4611b3323eb59434e5bd3f67eae4c180905504
Lars Bilke <lars.bilke@ufz.de>
2013-11-05 09:57:46 +0100

    Mavericks fixes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,22 +8,14 @@ class qt {
   include homebrew
   include xquartz
 
-  case $::macosx_productversion_major {
-    '10.9': {
-      warning('QT is currently unsupported by OS X Mavericks, not actually installing it!')
-
-      Package <| title == 'boxen/brews/qt' |> {
-        ensure => absent,
-      }
-    }
-
     default: {
       homebrew::formula { 'qt':
         before => Package['boxen/brews/qt'],
       }
 
       package { 'boxen/brews/qt':
-        ensure  => '4.8.5-boxen1',
+        ensure  => '4.8.5-boxen2',
+        install_options => [ '--HEAD' ],
         require => Class['xquartz'],
       }
     }
